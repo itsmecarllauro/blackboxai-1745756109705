1e13b048d7ccaf1728db684645371250f87c1278
Cee🦋 <149859757+itsmecarllauro@users.noreply.github.com>
2025-04-28 20:11:36 +0800

    Delete update_sales_table_add_sale_order_id.sql

--- update_sales_table_add_sale_order_id.sql
+++
@@ -1,8 +0,0 @@
--- SQL script to add sale_order_id column to sales table and set up foreign key constraint
-
-ALTER TABLE sales
-ADD COLUMN sale_order_id INT NULL AFTER id;
-
-ALTER TABLE sales
-ADD CONSTRAINT fk_sale_order
-FOREIGN KEY (sale_order_id) REFERENCES sales_orders(id) ON DELETE CASCADE;
